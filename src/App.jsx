fa3424021d8228b5095487b9c97b536d699f8aab
Dheeraj233034 <158183051+Dheeraj233034@users.noreply.github.com>
2025-03-15 10:13:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,59 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import AboutUs from "./components/AboutUs";
+import Stats from "./components/Stats";
+import Form from "./components/Form";
+import Testimonials from "./components/Testimonials";
+import ProjectComponent from "./components/Project";
+import ServicesComponent from "./components/Services";
+import ProductsComponent from "./components/Products";
+import OurVision from "./components/OurVision";
+import OurMission from "./components/OurMission";
+import Gallery from "./pages/Gallery"; // ✅ Use the correct Gallery component
+import TeamMembers from "./components/TeamMembers";
+import OurClients from "./components/OurClients";
+import OfficeLocations from "./components/OfficeLocations";
+import IndiaMap from "./components/Indiamap";
+import ConstructionInfo from "./components/ConstructionInfo";
 
-import './App.css'
-import AboutUs from './components/Aboutus'
-import ConstructionInfo from './components/Constructioninfo'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Stats from './components/Stats'
-import Form from './components/Form'
-import Testimonials from './components/Testimonials'
-import ProjectComponent from './components/Project'
-import SevicesComponent from './components/Services'
-import ProductsComponent from './components/Products'
-import OurVision from './components/OurVision'
-import OurMission from './components/OurMission'
-import Gallery from './components/Gallery'
-import TeamMembers from './components/TeamMembers'
-import OurClients from './components/OurClients'
-import OfficeLocations from './components/OfficeLocations'
-import IndiaMap from './components/Indiamap'
-
-
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-    <Navbar/>
-    <Hero/>
-    <AboutUs/>
-    <OurVision/>
-    <OurMission/>
-    <ConstructionInfo/>
-    <ProjectComponent/>
-    <SevicesComponent/>
-    <ProductsComponent/>
-    <Gallery/>
-    <TeamMembers/>
-    <Testimonials/>
-    <OurClients/>
-    <Stats/>
-    <Form/>
-    <IndiaMap/>
-    <OfficeLocations/>
-    <Footer/>
-    </>
-  )
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Home Route */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <AboutUs />
+              <OurVision />
+              <OurMission />
+              <ConstructionInfo />
+              <ProjectComponent />
+              <ServicesComponent />
+              <ProductsComponent />
+              <TeamMembers />
+              <Testimonials />
+              <OurClients />
+              <Stats />
+              <Form />
+              <IndiaMap />
+              <OfficeLocations />
+            </>
+          }
+        />
 
-export default App
+        {/* Gallery Page */}
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
